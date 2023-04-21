03a57fa21c92e07ecfcd4ee5aa758e636d33720c
YumaIto <yuma.ito@activ8.co.jp>
2023-04-21 11:09:58 +0900

    コミット漏れを追加

---
+++ Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class LineDrawer : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Material lineMat;
+    private LineRenderer _lineRenderer;
+
+    private LineRenderer LineRenderer
+    {
+        get
+        {
+            if (_lineRenderer == null)
+            {
+                _lineRenderer = gameObject.AddComponent<LineRenderer>();
+                _lineRenderer.useWorldSpace = false;
+                _lineRenderer.material = lineMat;
+            }
+
+            return _lineRenderer;
+        }
+    }
+    private int _positionCount;
+
+    private void Start()
+    {
+        // _lineRenderer = GetComponent<LineRenderer>();
+        // ラインの座標指定を、このラインオブジェクトのローカル座標系を基準にするよう設定を変更
+        // この状態でラインオブジェクトを移動・回転させると、描かれたラインもワールド空間に取り残されることなく、一緒に移動・回転
+        // _lineRenderer.useWorldSpace = false;
+        _positionCount = 0;
+        // mainCamera = Camera.main;
+    }
+
+    public void Draw(Vector3 position)
+    {
+        var cameraTransform = mainCamera.transform;
+        transform.position = cameraTransform.position + cameraTransform.forward * 10;
+        transform.rotation = cameraTransform.rotation;
+        
+        var pos = mainCamera.ViewportToScreenPoint(position);
+        pos.z = 10;
+        pos = mainCamera.ScreenToWorldPoint(pos);
+        pos = transform.InverseTransformPoint(pos);
+
+        // さらにそれをローカル座標に直す。
+        // pos = transform.InverseTransformPoint(pos);
+
+        // 得られたローカル座標をラインレンダラーに追加する
+        _positionCount++;
+        LineRenderer.positionCount = _positionCount;
+        LineRenderer.SetPosition(_positionCount - 1, pos);
+    }
+
+    public void Clear()
+    {
+        _positionCount = 0;
+    }
+}
--- Assets/Scripts/OscReceiver.cs
+++ Assets/Scripts/OscReceiver.cs
@@ -1,11 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using OscJack;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class OscReceiver : MonoBehaviour
 {
     [SerializeField] int port = 8000;
+    [SerializeField] private Camera targetCamera;
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private LineDrawer lineDrawer;
+    
+    // public UnityEvent<Vector3> OnReceivedPosition = new UnityEvent<Vector3>();
+
+    public bool IsReceivedStart = false;
+    private float lastReceivedTime = 0.0f;
+    [SerializeField] private float waitTime = 0.1f;
     
     // Start is called before the first frame update
     private void Start()
@@ -18,8 +30,22 @@ public class OscReceiver : MonoBehaviour
             });
     }
 
+    private void Update()
+    {
+        if (IsReceivedStart && lastReceivedTime - Time.time > waitTime)
+        {
+            IsReceivedStart = false;
+        }
+    }
+
     public void OnReceive(Vector2 position)
     {
-        Debug.Log(position);
-    }
+        lineDrawer.Draw(position);
+        // Debug.Log(position);
+        // var screen = targetCamera.ViewportToScreenPoint(position);
+        // Debug.Log(screen);
+        // screen.z = 10;
+        // var world = targetCamera.ScreenToWorldPoint(screen);
+        // Debug.Log(world);
+    } 
 }
